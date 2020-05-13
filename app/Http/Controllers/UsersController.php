6f17a138dd9eac3f0d09a5aaaab79b93c6fe6dda
Agung Yudha Berliantara <agung.berliantara@gmail.com>
2020-05-13 12:47:42 +0700

    Remove files dependency on Models\Users

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Users;
+use App\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -41,7 +41,7 @@ class UsersController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Users  $users
+     * @param  \App\User  $users
      * @return \Illuminate\Http\Response
      */
     public function show(Users $users)
@@ -52,7 +52,7 @@ class UsersController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Users  $users
+     * @param  \App\User  $users
      * @return \Illuminate\Http\Response
      */
     public function edit(Users $users)
@@ -64,7 +64,7 @@ class UsersController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Users  $users
+     * @param  \App\User  $users
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Users $users)
@@ -75,7 +75,7 @@ class UsersController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Users  $users
+     * @param  \App\User  $users
      * @return \Illuminate\Http\Response
      */
     public function destroy(Users $users)
